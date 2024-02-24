69fca9191b0c37650ba6afcb496f64dc1a8626ee
Malika17 <malikasharma1711@gmail.com>
2024-02-24 15:54:34 +0530

    text animation done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const parallax_el = document.querySelectorAll(".parallax");
+const main = document.querySelector("main");
 let xValue = 0;
 let yValue = 0;
 let rotateDegree = 0;
@@ -30,3 +31,32 @@ window.addEventListener("mousemove", (e) => {
   rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
   update(e.clientX);
 });
+
+if (window.innerWidth >= 725) {
+  main.style.maxHeight = `${window.innerWidth * 0.9}px`;
+} else {
+  main.style.maxHeight = `${window.innerWidth * 0.9}px`;
+}
+
+let timeline = gsap.timeline();
+
+timeline
+  .from(
+    ".text h1",
+    {
+      y:
+        window.innerHeight -
+        document.querySelector(".text h1").getBoundingClientRect().top,
+      duration: 2.5,
+    },
+    "1"
+  )
+  .from(
+    ".text h2",
+    {
+      y: -150,
+      opacity: 0,
+      duration: 1.5,
+    },
+    "1.5"
+  );
